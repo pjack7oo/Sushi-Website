570eb0194cfb5b5338919c0bf08368a50c05a2cb
pjack7oo <pjackowski@luc.edu>
2020-03-17 01:32:10 -0500

    ricecooker stores rice

--- src/game/objects/riceCooker.js
+++ src/game/objects/riceCooker.js
@@ -1,6 +1,9 @@
 import * as shapes from '../utils/shapes.js'
 import * as drawing from '../utils/drawing.js';
 import * as ingredients from '../objects/ingredients.js';
+import * as ioControl   from '../utils/iocontrol.js';
+
+
 
 export const riceCooker = {
     type: shapes.shapeType.RECTANGLE,
@@ -14,7 +17,26 @@ export const riceCooker = {
     lineWidth: 1,
     image: null,
     startTime: 0,
-    isActive: false
+    isActive: false,
+    riceCount: 4,
+    rice: null
+
+}
+
+export function checkRiceShape(mouse, context) {
+    
+    drawing.drawShape(context, riceCooker.rice);
+    let ret;
+                
+    ret = ioControl.moveItem(mouse, riceCooker.rice);
+                
+
+    if (ret) {
+        return ret;
+    }
+    
+        
+    return false;
 }
 
 export function drawRiceCooker(ctx)
@@ -30,9 +52,25 @@ export function drawRiceCooker(ctx)
     ctx.ellipse(riceCooker.x + radiusX, riceCooker.y + radiusY, radiusX, radiusY, 0, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
+    drawing.printAtWordWrap(ctx, riceCooker.riceCount.toString(), 
+            riceCooker.x + riceCooker.w/2, riceCooker.y + riceCooker.h/1.5,10,20, "Red", '20px Arial', 'Center');
+}
+
+export function drawRice(context) {
+    drawing.drawShape(context, riceCooker.rice.renderType);
+}
+
+export function addRice(rice) {
+    riceCooker.rice = rice;
+    riceCooker.riceCount += 1;
+}
+
+export function removeRice(rice) {
+    createRice();
+    riceCooker.riceCount -=1;
 }
 
 export function createRice()
 {
-    ingredients.createRice(riceCooker.x + 18, riceCooker.y + 9);
+    riceCooker.rice = ingredients.createRice(riceCooker.x + 18, riceCooker.y + 9);
 }
