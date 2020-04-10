d10acb97e3040d305cab7bdc5f89ce4144282d8e
pjack7oo <pjackowski@luc.edu>
2020-04-10 04:14:32 -0500

    mongodb being setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-const express = require('express');
+//Server routes and controllers were built from tutorial https://jorgeramon.me/the-meeting-room-booking-app-tutorial/
+
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      mongoose   = require('mongoose'),
+      expressSession = require('express-session'),
+      MongoStore    = require('connect-mongo')(expressSession),
+      accountRoutes = require('../server/routes/account');
+
 const path = require('path');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
+
+
+var app = express();
 
 const publicDir = path.join(__dirname, '../');
 
@@ -32,9 +43,34 @@ router.get('/',function(req,res){
   //     console.log('Http server closed.');
   //   });
   // });
-var app = express();
-app.use(express.static(path.join(publicDir, 'public')));
-app.use('/', router);
+  app.use(express.static(path.join(publicDir, 'public')));
+  app.use('/', router);
+
+var dbName = 'SushiCatDB';
+var connectionString = 'mongodb://localhost:27017/' + dbName;
+
+mongoose.connect(connectionString);
+mongoose.once = function() {
+  console.log(true);
+};
+
+app.use(expressSession({
+  secret: 'foo',
+  resave: true,
+  store : new MongoStore({
+    mongooseConnection: mongoose,
+    secret: 'bar',
+    autoRemove: 'disabled'
+  }),
+  saveUninitialized: true
+
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/api', accountRoutes)
+
+
 
 var server = app.listen(PORT, function() {
   console.log(`Listening on ${ PORT }`);
