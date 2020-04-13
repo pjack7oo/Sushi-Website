d54e448bb7346b74d7af22dcc0f1f4b5d39c2f66
pjack7oo <pjackowski@luc.edu>
2020-04-13 00:17:38 -0500

    fixed server

--- server/index.js
+++ server/index.js
@@ -21,8 +21,31 @@ router.get('/',function(req,res){
   res.sendFile(path.join(publicDir + '/public/main2.html'));
   //__dirname : It will resolve to your project folder.
 });
+router.get('/about', function(req, res) {
+  res.sendStatus(404);
+});
 
+// router.route('/api')
+// .get("/account/register", function(req, res) {
+//   res.send("hello world");
+// });
+// router.post(function(req, res) {
+//         console.log("register");
+        
+//         var accountController = new AccountController(User, req.session, mailer);
+//         var userRegistration  = new UserRegistration(req.body);
+//         var apiResponse1 = accountController.getUserFromRegistration(userRegistration);
 
+//         res.set("Access-Control-Allow-Origin", url);
+
+//         if (apiResponse1.success) {
+//             accountController.register(apiResponse1.extras.user, function (err, apiResponse2) {
+//                 return res.send(apiResponse2);
+//             });
+//         } else {
+//             res.send(apiResponse1);
+//         }
+//     });
 // var app = express()
 //   .use(express.static(path.join(publicDir, 'public')))
 //   .use('/', router)
@@ -43,22 +66,37 @@ router.get('/',function(req,res){
   //     console.log('Http server closed.');
   //   });
   // });
+  // app.use(function(req, res, next){
+  //   res.sendStatus(404);
+  
+  //   res.format({
+  //     html: function () {
+  //       res.render('404', { url: req.url })
+  //     },
+  //     json: function () {
+  //       res.json({ error: 'Not found' })
+  //     },
+  //     default: function () {
+  //       res.type('txt').send('Not found')
+  //     }
+  //   })
+  // });
+
   app.use(express.static(path.join(publicDir, 'public')));
-  app.use('/', router);
+  
 
 var dbName = 'SushiCatDB';
 var connectionString = 'mongodb://localhost:27017/' + dbName;
 
 mongoose.connect(connectionString);
-mongoose.once = function() {
-  console.log(true);
-};
+
 
 app.use(expressSession({
+  ker: 'session',
   secret: 'foo',
   resave: true,
   store : new MongoStore({
-    mongooseConnection: mongoose,
+    mongooseConnection: mongoose.connection,
     secret: 'bar',
     autoRemove: 'disabled'
   }),
@@ -68,10 +106,16 @@ app.use(expressSession({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use('/api', accountRoutes)
 
+// app.get('/404', function(req, res, next){
+//   // trigger a 404 since no other middleware
+//   // will match /404 after this one, and we're not
+//   // responding here
+//   next();
+// });
 
-
+app.use('/', router);
+app.use('/api', accountRoutes);
 var server = app.listen(PORT, function() {
   console.log(`Listening on ${ PORT }`);
 })
