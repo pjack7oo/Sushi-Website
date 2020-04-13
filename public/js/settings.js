3dd6dc127cd2e0f9715af80406c11d5cc8c2c5a9
pjack7oo <pjackowski@luc.edu>
2020-04-13 02:34:10 -0500

    sushi cat settings

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,2 +1,5 @@
 var SushiCat = SushiCat || {};
-SushiCat.Settings = "http://localhost:5000/api/account/register";
+SushiCat.Settings.signIn = "http://localhost:5000/api/account/register";
+SushiCat.Settings.signUp = "http://localhost:5000/api/account/login";
+SushiCat.Settings.sessionTimeout = 1000*60*60*24;
+
