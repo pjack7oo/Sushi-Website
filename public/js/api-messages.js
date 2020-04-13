a8d28dbe2aa03da656ad84a6966fcbdd2c74d95e
pjack7oo <pjackowski@luc.edu>
2020-04-13 00:15:05 -0500

    added extra msg for session

--- public/js/api-messages.js
+++ public/js/api-messages.js
@@ -1,14 +1,15 @@
 var SushiCat = SushiCat || {};
 
 SushiCat.ApiMessages = SushiCat.ApiMessages || {};
-SushiCat.ApiMessages.EMAIL_NOT_FOUND = 0;
+SushiCat.ApiMessages.USERNAME_NOT_FOUND = 0;
 SushiCat.ApiMessages.INVALID_PWD = 1;
 SushiCat.ApiMessages.DB_ERROR = 2;
 SushiCat.ApiMessages.NOT_FOUND = 3;
-SushiCat.ApiMessages.EMAIL_ALREADY_EXISTS = 4;
+SushiCat.ApiMessages.USERNAME_ALREADY_EXISTS = 4;
 SushiCat.ApiMessages.COULD_NOT_CREATE_USER = 5;
 SushiCat.ApiMessages.PASSWORD_RESET_EXPIRED = 6;
 SushiCat.ApiMessages.PASSWORD_RESET_HASH_MISMATCH = 7;
 SushiCat.ApiMessages.PASSWORD_RESET_EMAIL_MISMATCH = 8;
 SushiCat.ApiMessages.COULD_NOT_RESET_PASSWORD = 9;
 SushiCat.ApiMessages.PASSWORD_CONFIRM_MISMATCH = 10;
+SushiCat.ApiMessages.COULD_NOT_CREATE_SESSION      = 11;
