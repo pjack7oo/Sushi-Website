48617155e729f39fbcac73bf173cf0e689f07849
pjack7oo <pjackowski@luc.edu>
2020-04-13 00:16:28 -0500

    fixed register

--- server/routes/account.js
+++ server/routes/account.js
@@ -2,32 +2,46 @@ const express = require('express'),
       router  = express.Router(),
       AccountController = require('../controllers/account.js'),
       ApiResponse       = require('../models/api-response.js'),
-      UserRegistration  = require('../models/user-registration.js');
-      //user            //TODO  
-      //mailer          //TODO or remove
+      UserRegistration  = require('../models/user-registration.js'),
+      User              = require('../models/user.js'),
+      UserSession       = require('../models/user-session.js'),
+      UserLogon         = require('../models/user-logon.js');
+      MailerMock        = require("../test/mailer-mock.js"),
+      mailer = new MailerMock();      //TODO or remove
 var session = [],
     url = "http://localhost:42550";
 
-router.route('./account/register')
+router.route('/account/register')
+    .get(function(req, res) {
+        res.send("hello");
+    })
     .post(function(req, res) {
+        
+        
         var accountController = new AccountController(User, req.session, mailer);
         var userRegistration  = new UserRegistration(req.body);
+        
         var apiResponse1 = accountController.getUserFromRegistration(userRegistration);
 
         res.set("Access-Control-Allow-Origin", url);
 
         if (apiResponse1.success) {
+            
+            
             accountController.register(apiResponse1.extras.user, function (err, apiResponse2) {
+                
                 return res.send(apiResponse2);
             });
         } else {
             res.send(apiResponse1);
         }
     });
+    
 
 router.route('/account/logon')
     .post(function (req, res) {
-        var accountController = new AccountController(User, req.session, mailer);
+        var userSession   = new UserSession(),
+        accountController = new AccountController(User, req.session, userSession, mailer);
 
         var userLogon = new userLogon(req.body);
 
