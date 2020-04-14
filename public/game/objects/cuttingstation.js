c166ea7d30f9c6e50174b102c1dd0364bcd483bd
pjack7oo <pjackowski@luc.edu>
2020-04-14 04:40:31 -0500

    cutting turned into a class, 2 boards

--- public/game/objects/cuttingstation.js
+++ public/game/objects/cuttingstation.js
@@ -15,30 +15,118 @@ const shapeType = {
 var boardImage = new Image();
 boardImage.src = "./game/images/TexturesCom_CloseupBambooCuttingBoard_M.png";
 
-export const cuttingStation = {
-  type: shapeType.RECTANGLE,
-  x: 410,
-  y: 400,
-  w: 150,
-  h: 100,
-  intColor: "#C19A6B",
-  outColor: "#C19A6B",
-  fill: true,
-  lineWidth: 1,
-  image: boardImage,
-  item: null,
-  startTime: 0,
-  isActive: false,
-  cuttingSpeed: 8, //seconds ,
-  cuttingSpeedCost: 500,
-  progress: 0
-};
-var bar = new progBar.progressbar(
-  cuttingStation.x,
-  cuttingStation.y - 60,
-  cuttingStation.w,
-  20
-);
+export class CuttingStation{
+  constructor(x, y, w, h) {
+    this.type = shapeType.RECTANGLE;
+    this.x    = x;
+    this.y    = y;
+    this.w    = w;
+    this.h    = h;
+    this.intColor = "#C19A6B";
+    this.outColor = "#C19A6B";
+    this.fill     = true;
+    this.lineWidth = 1;
+    this.image     = boardImage;
+    this.item      = null;
+    this.startTime = 0;
+    this.isActive  = false;
+    this.cuttingSpeed = 8;
+    this.cuttingSpeedCost = 500;
+    this.progress         = 0;
+  }
+  checkCutRoll() {
+    if (this.isActive == true) {
+      let currentTime = performance.now(),
+        elapsedTime = currentTime - this.startTime,
+        percentComplete =
+          (elapsedTime / (this.cuttingSpeed * 1000)) * 100;
+      this.progress = percentComplete;
+      drawing.Invalidate();
+  
+      if (percentComplete >= 100) {
+          if (this.item instanceof Fish) {
+              this.isActive = false;
+              this.startTime = 0;
+              ingredientBox.fillBoxes(this.item.fishType);
+              this.item = null;
+              drawing.Invalidate();
+          }
+          else {
+              this.isActive = false;
+              this.startTime = 0;
+              this.item.isCut = true;
+              this.item.canEnterPlate = true;
+              this.item.canEnterCuttingStation = false;
+              this.item.renderType.w = this.item.radius * 4;
+              this.item.renderType.h = this.item.radius * 4;
+              // cuttingStation.item.renderType.x -= cuttingStation.item.radius*2;
+              // cuttingStation.item.renderType.y -= cuttingStation.item.radius*2;
+              //roll.downSizeRoll(cuttingStation.item);
+              roll.pushRoll(this.item);
+              this.item = null;
+  
+              console.log("Cut roll");
+  
+              drawing.Invalidate();
+          }
+        
+      }
+    }
+  }
+
+  checkCuttingStation(mySelect) {
+    if (mySelect != null) {
+      if (mySelect.canEnterCuttingStation == false) {
+        drawing.Invalidate();
+        return;
+      }
+      if (mySelect.isCut == true) {
+        drawing.Invalidate();
+        return;
+      }
+      if (
+        shapes.Contains(this, mySelect.renderType) &&
+        this.item == null
+      ) {
+        this.item = mySelect;
+        console.log(this.item);
+        roll.removeRoll(mySelect);
+      }
+    }
+  }
+}
+var cuttingStation = CuttingStation, cuttingStation2 = cuttingStation;
+
+export function cuttingStationInit() {
+  cuttingStation = new CuttingStation(350,400,150,100);
+  bar = new progBar.progressbar(
+    cuttingStation.x,
+    cuttingStation.y - 60,
+    cuttingStation.w,
+    20
+  );
+  //cuttingStation2 = new CuttingStation(510, 400, 150, 100);
+}
+// const cuttingStatio1n = {
+//   type: shapeType.RECTANGLE,
+//   x: 410,
+//   y: 400,
+//   w: 150,
+//   h: 100,
+//   intColor: "#C19A6B",
+//   outColor: "#C19A6B",
+//   fill: true,
+//   lineWidth: 1,
+//   image: boardImage,
+//   item: null,
+//   startTime: 0,
+//   isActive: false,
+//   cuttingSpeed: 8, //seconds ,
+//   cuttingSpeedCost: 500,
+//   progress: 0
+// };
+var bar = progBar.progressbar, bar2 = progBar.progressbar;
+ 
 
 export function getData() {
   var cuttingStationData = {};
@@ -68,6 +156,15 @@ export function upgradeCuttingSpeed() {
 
 export function buyNewCuttingBoard() {
   if (player.hasEnoughMoney(2000)) {
+    cuttingStation2 = new CuttingStation(510, 400, 150, 100);
+    bar2 = new progBar.progressbar(
+      cuttingStation2.x,
+      cuttingStation2.y - 60,
+      cuttingStation2.w,
+      20
+    );
+    player.removeMoney(2000);
+
     //todo buy cutting board
   }
 }
@@ -76,44 +173,10 @@ export function getSecondBoardCost() {
 }
 
 export function checkCutRoll() {
-  if (cuttingStation.isActive == true) {
-    let currentTime = performance.now(),
-      elapsedTime = currentTime - cuttingStation.startTime,
-      percentComplete =
-        (elapsedTime / (cuttingStation.cuttingSpeed * 1000)) * 100;
-    cuttingStation.progress = percentComplete;
-    drawing.Invalidate();
-
-    if (percentComplete >= 100) {
-        if (cuttingStation.item instanceof Fish) {
-            cuttingStation.isActive = false;
-            cuttingStation.startTime = 0;
-            ingredientBox.fillBoxes(cuttingStation.item.fishType);
-            cuttingStation.item = null;
-            drawing.Invalidate();
-        }
-        else {
-            cuttingStation.isActive = false;
-            cuttingStation.startTime = 0;
-            cuttingStation.item.isCut = true;
-            cuttingStation.item.canEnterPlate = true;
-            cuttingStation.item.canEnterCuttingStation = false;
-            cuttingStation.item.renderType.w = cuttingStation.item.radius * 4;
-            cuttingStation.item.renderType.h = cuttingStation.item.radius * 4;
-            // cuttingStation.item.renderType.x -= cuttingStation.item.radius*2;
-            // cuttingStation.item.renderType.y -= cuttingStation.item.radius*2;
-            //roll.downSizeRoll(cuttingStation.item);
-            roll.pushRoll(cuttingStation.item);
-            cuttingStation.item = null;
-
-            console.log("Cut roll");
-
-            drawing.Invalidate();
-        }
-      
-    }
+  cuttingStation.checkCutRoll();
+  if (cuttingStation2 instanceof CuttingStation) {
+    cuttingStation2.checkCutRoll();
   }
-
   //drawProgressBar(cuttingStation.x, cuttingStation.y, cuttingStation.w, 20, precentComplete);
   //todo implement cutAnimation
 }
@@ -126,6 +189,47 @@ export function drawCuttingStation(context) {
     cuttingStation.h,
     cuttingStation.image
   );
+  if (cuttingStation2 instanceof CuttingStation) {
+    drawing.drawRoundRectImage(
+      cuttingStation2.x,
+      cuttingStation2.y,
+      cuttingStation2.w,
+      cuttingStation2.h,
+      cuttingStation2.image
+    );
+
+    if (cuttingStation2.item != null) {
+      if (cuttingStation2.isActive) {
+        drawing.drawTextBox(
+          context,
+          cuttingStation2.x,
+          cuttingStation2.y - 40,
+          cuttingStation2.w,
+          40,
+          "Cutting!"
+        );
+        progBar.drawColorProgressbar(cuttingStation2.progress, bar2, false);
+      } else {
+        drawing.drawTextBox(
+          context,
+          cuttingStation2.x,
+          cuttingStation2.y - 40,
+          cuttingStation2.w,
+          40,
+          "Ready to cut! Press C key"
+        );
+      }
+      //drawing.drawRoundRectImage(cuttingStation.x, cuttingStation.y, cuttingStation.w, cuttingStation.h, cuttingStation.image);
+      if (cuttingStation2.item instanceof Fish) {
+          cuttingStation2.item.draw();
+      }
+      else {
+          drawing.drawShape(context, cuttingStation2.item.renderType);
+      }
+      
+      drawing.Invalidate();
+    }
+  }
 
   if (cuttingStation.item != null) {
     if (cuttingStation.isActive) {
@@ -158,51 +262,67 @@ export function drawCuttingStation(context) {
     
     drawing.Invalidate();
   }
+  
+
+  
 }
 
 export function cutRoll() {
+  if (cuttingStation2 instanceof CuttingStation) {
+    if (cuttingStation2.item == null) {
+      
+    }
+    if (cuttingStation2.isActive == false && cuttingStation2.item != null) {
+      cuttingStation2.isActive = true;
+      cuttingStation2.startTime = performance.now();
+      
+    }
+    
+  } 
+
   if (cuttingStation.item == null) {
     return;
   }
-  if (cuttingStation.isActive == false) {
+  if (cuttingStation.isActive == false && cuttingStation.item != null) {
     cuttingStation.isActive = true;
     cuttingStation.startTime = performance.now();
   }
+  
+
+  
+  
   drawing.Invalidate();
 }
 
 export function checkCuttingStation(mySelect) {
   //console.log(cuttingStation.item);
 
-  if (mySelect != null) {
-    if (mySelect.canEnterCuttingStation == false) {
-      drawing.Invalidate();
-      return;
-    }
-    if (mySelect.isCut == true) {
-      drawing.Invalidate();
-      return;
-    }
-    if (
-      shapes.Contains(cuttingStation, mySelect.renderType) &&
-      cuttingStation.item == null
-    ) {
-      cuttingStation.item = mySelect;
-      console.log(cuttingStation.item);
-      roll.removeRoll(mySelect);
-    }
+  cuttingStation.checkCuttingStation(mySelect);
+
+  if (cuttingStation2 instanceof CuttingStation) {
+    cuttingStation2.checkCuttingStation(mySelect);
   }
 }
 
-export function hasNoItem() {
-  return cuttingStation.item == null;
+export function hasNoItem(num) {
+  if (num == 0 ) {
+    return cuttingStation.item == null;
+  }
+  if (num == 1) {
+    return cuttingStation2.item == null;
+  }
 }
 
-export function insertFish(Fish) {
-  if (hasNoItem()) {
+export function insertFish(Fish, num) {
+  if (num == 0) {
     cuttingStation.item = Fish;
     cuttingStation.item.x = cuttingStation.x;
     cuttingStation.item.y = cuttingStation.y;
+  }
+  if (num == 1) {
+    cuttingStation2.item = Fish;
+    cuttingStation2.item.x = cuttingStation2.x;
+    cuttingStation2.item.y = cuttingStation2.y;
   }
 }
 
