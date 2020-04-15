4489f1d676b6b326e686b84ffca86d3cc29df4c8
pjack7oo <pjackowski@luc.edu>
2020-04-14 21:32:45 -0500

    send save data

---
+++ server/models/user-sendSave.js
@@ -0,0 +1,9 @@
+var UserSendSave = function(cnf) {
+    this.username  = cnf.username,
+    this.userMoney = cnf.money,
+    this.date      = cnf.date,
+    this.gameData  = cnf.gameData
+
+};
+
+module.exports = UserSendSave;
