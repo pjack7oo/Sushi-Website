271e6608312863117a678bf8ea7330457094eb19
pjack7oo <pjackowski@luc.edu>
2020-04-19 01:03:05 -0500

    removed settings.js

--- public/js/settings.js
+++
@@ -1,5 +0,0 @@
-var SushiCat = SushiCat || {};
-SushiCat.Settings.signIn = "http://localhost:5000/api/account/register";
-SushiCat.Settings.signUp = "http://localhost:5000/api/account/login";
-SushiCat.Settings.sessionTimeout = 1000*60*60*24;
-
