5561f2fed28a8eac7f7116afbb6ef4debbfc688d
pjack7oo <pjackowski@luc.edu>
2020-04-26 21:47:02 -0500

    acces control

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ const publicDir = path.join(__dirname, "../");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
